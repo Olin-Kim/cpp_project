a99903da32085019d0062acd77d703bbe223bf60
olin-kim <Olin.Kim@obigo.com>
2017-04-10 16:35:33 +0900

    oop1 :: mod3

--- bank.cpp
+++ bank.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string.h>
 
 using std::cout;
 using std::cin;
@@ -21,7 +22,7 @@ typedef struct Account{
 
 }Account;
 
-void showAccountList(Account (&accList)[100],int len){
+void ShowAccountList(Account (&accList)[100],int len){
 	for(int i=0;i<len;i++){
 		cout<<"===== "<<accList[i].name<<" ====="<<endl;
 		cout<<"Account : "<<accList[i].account<<endl;
@@ -30,16 +31,28 @@ void showAccountList(Account (&accList)[100],int len){
 	}
 }
 
-void showMenu(){
+Account* SearchAccountId(Account (& accountList)[100],char (&accountId)[50], int accountListLen){
+	for(int i=0;i<accountListLen;i++){
+		if(strcmp(accountList[i].account,accountId)==0){
+			return &(accountList[i]);
+		}
+	}
+	return NULL;
+}
+
+void ShowMenu(){
 	cout<<"========= Menu ========="<<endl;
 	cout<<" Open Account      - 1"<<endl;
 	cout<<" Deposit Money     - 2"<<endl;
 	cout<<" Withdraw Money    - 3"<<endl;
 	cout<<" Show Balance      - 4"<<endl;
 	cout<<" Show Account List - 5"<<endl;
+	cout<<" EXIT              - 6"<<endl;
 	cout<<"========================"<<endl;
 
 }
+
+
 
 int main(void){
 	
@@ -49,9 +62,11 @@ int main(void){
 
 	BANK_START:
 	while(true){
-		showMenu();
+		ShowMenu();
 		cout<< "input menu>> ";
 		cin>>selectNumber;
+
+		
 
 		if(cin.fail()){
 			cout<<"cin Failure!!!"<<endl;
@@ -65,21 +80,81 @@ int main(void){
 				cin>>accountList[accountListLen].account;
 				cout<<"Input your name: ";
 				cin>>accountList[accountListLen].name;
-				accountList[accountListLen].balance=0;
+				cout<<"Input your money: ";
+				cin>>accountList[accountListLen].balance;
 				accountListLen++;
 				break;
+
 				case DEPOSIT:
-				cout<<"DEPOSIT"<<endl;
+				{
+					char accountId[50];
+					int money=0;
+					Account* searchedAccount=NULL;
+					
+					cout<<"DEPOSIT"<<endl;
+					cout<<"Input your account: ";
+					cin>>accountId;
+					cout<<"Deposit your money: ";
+					cin>>money;
+
+					searchedAccount=SearchAccountId(accountList, accountId, accountListLen);
+					if(searchedAccount!=NULL){
+						(*searchedAccount).balance+=money;
+						cout<<"Deposit Success!!!"<<endl;
+					}else{
+						cout<<"No Account!!!"<<endl;
+					}
+				}
+
 				break;
+
 				case WITHDRAW:
 				cout<<"WITHDRAW"<<endl;
+				{
+					char accountId[50];
+					int money=0;
+					Account* searchedAccount=NULL;
+					
+					cout<<"Input your account: ";
+					cin>>accountId;
+					cout<<"Withdraw your money: ";
+					cin>>money;
+
+					searchedAccount=SearchAccountId(accountList, accountId, accountListLen);
+					if(searchedAccount!=NULL){
+						(*searchedAccount).balance-=money;
+						cout<<"Withdraw Success!!!"<<endl;
+					}else{
+						cout<<"No Account!!!"<<endl;
+					}
+				}				
 				break;
+
 				case BALANCE:
 				cout<<"BALANCE"<<endl;
+				{
+					char accountId[50];
+					int money=0;
+					Account* searchedAccount=NULL;
+					
+					cout<<"Input your account: ";
+					cin>>accountId;
+					cout<<"Withdraw your money: ";
+					cin>>money;
+
+					searchedAccount=SearchAccountId(accountList, accountId, accountListLen);
+					if(searchedAccount!=NULL){
+						cout<<"My balance: "<<(*searchedAccount).balance<<endl;
+					}else{
+						cout<<"No Account!!!"<<endl;
+					}
+				}
 				break;
+
 				case SHOW_ACC_LIST:
-				showAccountList(accountList,accountListLen);
+				ShowAccountList(accountList,accountListLen);
 				break;
+				
 				case EXIT:
 				cout<<"EXIT"<<endl;
 				return 0;
